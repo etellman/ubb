349cea59c71c2325529d83a7cfe3a8b634a4a1d3
Edwin Tellman <etellman@pobox.com>
2013-12-26 14:18:08 -0800

    math 146 week 1

---
+++ sccc/math146/week01/hw/hw01.R
@@ -0,0 +1,19 @@
+library(ggplot2) 
+library(reshape)
+library(xtable)
+
+# library(gcookbook)
+
+# exercise 26
+ex26 <- read.delim("data/ex01-26.dat", header = TRUE, sep = '\t')
+plot <- ggplot(ex26, aes(x = reorder(cause, -deaths), y = deaths)) + 
+  geom_bar(stat = "identity") +
+  labs(x = "Cause", y = "Deaths")
+  scale_fill_grey()
+
+print(plot)
+ggsave("ex26.eps")
+
+str(ex26)
+?xtable
+
