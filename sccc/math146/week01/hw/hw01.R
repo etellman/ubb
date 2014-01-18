54cd1288e46c76e9288387f43b7c598f1cd5d390
Edwin Tellman <etellman@pobox.com>
2014-01-18 12:10:54 -0800

    hw 1 solutions from 5th edition book

--- sccc/math146/week01/hw/hw01.R
+++ sccc/math146/week01/hw/hw01.R
@@ -2,105 +2,141 @@ library(ggplot2)
 library(reshape)
 library(xtable)
 
+# exercise 25
+ex25 <- read.delim("ex01-25.dat", header = TRUE, sep = '\t')
+other <- 100 - sum(ex25$Popular)
+
+colors <- c(as.vector(ex25$Color), "Other")
+colors
+
+ex25$Color <- factor(colors)
+ex25 <- rbind(ex25, data.frame(Color = "Other", Popular = 5))
+
+str(ex25)
+
+plot <- ggplot(ex25, aes(x = reorder(Color, -Popular), y = Popular)) + 
+  geom_bar(stat = "identity", fill="lightblue", color = "black") +
+  labs(x = "Color", y = "Percentage") +
+  ggtitle("Car and Truck Colors")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex25.eps", width = 4, height = 2.5)
+
 # exercise 26
-ex26 <- read.delim("data/ex01-26.dat", header = TRUE, sep = '\t')
-plot <- ggplot(ex26, aes(x = reorder(cause, -deaths), y = deaths)) + 
+ex26 <- read.delim("ex01-26.dat", header = TRUE, sep = '\t')
+plot <- ggplot(ex26, aes(x = reorder(Age, Pct), y = Pct)) + 
   geom_bar(stat = "identity", fill="lightblue", color = "black") +
-  labs(x = "Cause", y = "Deaths")
-
+  labs(x = "Age", y = "Percentage") +
+  coord_flip() +
+  ggtitle("Online Music Purchasers")
 print(plot)
-ggsave("ex26.eps", width = 5, height = 3)
 
-# exercise 28
-ex28 <- read.delim("data/ex01-28.dat", header = TRUE, sep = '\t')
-plot <- ggplot(ex28, aes(x = age, y = percent)) + 
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex26.eps", width = 4, height = 2.5)
+
+# exercise 29
+ex29 <- read.delim("ex01-29.dat", header = TRUE, sep = '\t')
+
+plot <- ggplot(ex29, aes(x = Type, y = Pct)) + 
   geom_bar(stat = "identity", fill="lightblue", color = "black") +
-  labs(x = "Age", y = "Movie Attendance")
-
-ggsave("ex28.eps", width = 5, height = 3)
+  labs(x = "Type", y = "Percentage") +
+  coord_flip() +
+  ggtitle("Spam")
 print(plot)
 
-# exercise 30
-ex30 <- read.delim("data/ex01-30.dat", header = TRUE, sep = '\t')
-ex30$servings <- as.factor(ex30$servings)
-plot <- ggplot(ex30, aes(x = servings, y = count)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Number of Subjects", y = "Servings per Day")
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex29a.eps", width = 4, height = 2.5)
+
+plot <- ggplot(ex29, aes(x = reorder(Type, Pct), y = Pct)) + 
+  geom_bar(stat = "identity", fill="lightblue", color = "black") +
+  labs(x = "Type", y = "Percentage") +
+  coord_flip() +
+  ggtitle("Spam")
 print(plot)
 
-ggsave("ex30.eps", width = 5, height = 3)
-
-cumsum(ex30$count)
-sum(ex30$count)/2
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex29b.eps", width = 4, height = 2.5)
 
 # exercise 34
-ex34 <- read.delim("data/ta01-04.dat", header = TRUE, sep = '\t')
-ex34 <- ex34[order(-ex34$MDs),]
-summary(ex34)
+ex34 <- read.delim("ta01-04.dat", header = TRUE, sep = '\t')
 
-plot <- ggplot(ex34, aes(x = MDs)) + 
-  geom_histogram(fill = "lightblue", color = "black", binwidth=50) +
-  labs(x = "MDs per 100,000 people", y = "Number of States")
+plot <- ggplot(ex34, aes(x = Ratio)) + 
+  geom_histogram(binwidth = 1, color = "black", fill = "lightblue") +
+  labs(x = "Ratio", y = "Count") +
+  ggtitle("Fatty Acids")
 print(plot)
-ggsave("ex34.eps", width = 5, height = 3)
 
-plot <- ggplot(ex34[1:25,], aes(y = reorder(state, MDs), x = MDs)) + 
-  geom_point() +
-  labs(y = "State", x = "MDs per 100,000 people")
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex34.eps", width = 4, height = 2.5)
+
+# exercise 35
+ex35 <- read.delim("ta01-05.dat", header = TRUE, sep = '\t')
+
+plot <- ggplot(ex35, aes(x = MDs)) + 
+  geom_histogram(binwidth = 50, color = "black", fill = "lightblue") +
+  labs(x = "Doctors per 100,000 People", y = "Count") +
+  ggtitle("Doctors")
 print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex35.eps", width = 4, height = 2.5)
 
 # exercise 37
-ex37 <- read.delim("data/ex01-37.dat", header = TRUE, sep = '\t')
+ex37 <- read.delim("ex01-37.dat", header = TRUE, sep = '\t')
 str(ex37)
 
-plot <- ggplot(data = ex37, aes(x = study, fill = sex)) + 
-  geom_histogram(color = "blue", binwidth = 50, position = "stack") +
-  labs(x = "Study Hours", y = "Number of Students") 
-
+plot <- ggplot(ex37, aes(x = sole)) + 
+  geom_histogram(binwidth = 500, color = "black", fill = "lightblue") +
+  labs(x = "Fish", y = "Number of Years") +
+  ggtitle("Rock Sole")
 print(plot)
-ggsave("ex37.eps", width = 5, height = 3)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex37.eps", width = 4, height = 2.5)
 
 # exercise 39
-ex39 <- read.delim("data/ex01-39.dat", header = TRUE, sep = '\t')
+ex39 <- read.delim("ex01-37.dat", header = TRUE, sep = '\t')
 str(ex39)
-ex39$rate <- apply(ex39, 1, function(row) as.integer(row["accidents"]) / as.integer(row["drivers"]))
-ex39$pot <- factor(x = ex39$pot, levels = c("never", "1to10", "11to50", "51plus"))
-ex39$pot
-plot <- ggplot(data = ex39, aes(x = pot, y = rate)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Marijuana Use", y = "Accidents per Driver") 
 
-print(plot)
-ggsave("ex39.eps", width = 5, height = 3)
-
-# exercise 41
-ex41 <- read.delim("data/ex01-41.dat", header = TRUE, sep = '\t')
-ex41long <- melt(ex41, id = "Year")
-str(ex41long)
-
-plot <- ggplot(ex41long, aes(x = Year, y = value, fill = variable)) + 
-  geom_bar(stat = "identity", position = "dodge", color = "black") +
-  scale_fill_grey()
-  labs(x = "Year", y = "Defects") 
+plot <- ggplot(ex39, aes(x = year, y = sole)) + 
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Recruitment") +
+  ggtitle("Rock Sole")
 print(plot)
 
-ggsave("ex41.eps", width = 5, height = 3)
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex39.eps", width = 4, height = 2.5)
 
-# exercise 44
-ex44 <- read.delim("data/ex01-44.dat", header = TRUE, sep = '\t')
-nrow(subset(ex44, attacks >= 10, select = c(attacks)))
+# exercise 40
+ex40.pot = factor(c("Never", "1-10 Times", "11-50 Times", "51+ Times"))
+ex40.drivers = c(452, 229, 70, 156)
+ex40.accidents = c(59, 36, 15, 50)
 
-plot <- ggplot(ex44, aes(x = year, y = attacks)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Year", y = "Attacks") 
+ex40 <- data.frame(pot = ex40.pot, drivers = ex40.drivers, accidents = ex40.accidents)
+str(ex40)
+
+plot <- ggplot(ex40, aes(x = reorder(pot, accidents / drivers), y = accidents / drivers)) + 
+  geom_bar(stat = "identity", fill="lightblue", color = "black") +
+  labs(x = "Pot Use", y = "Accidents per Driver") +
+  ggtitle("Accidents and Pot")
 print(plot)
 
-ggsave("ex44Time.eps", width = 5, height = 3)
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex40.eps", width = 4, height = 2.5)
 
-plot <- ggplot(ex44, aes(x = attacks)) + 
-  geom_histogram(fill = "lightblue", color = "black", binwidth = 5) +
-  labs(x = "Attacks", y = "Number of Years") 
+# exercise 45
+ex45 <- read.delim("ex01-45.dat", header = TRUE, sep = '\t')
+str(ex45)
+
+summary(ex45)
+nrow(subset(ex45, year >= 1986))
+
+plot <- ggplot(ex45, aes(x = bites)) + 
+  geom_histogram(binwidth = 2, color = "black", fill = "lightblue") +
+  labs(x = "Bites", y = "Number of Years") +
+  ggtitle("Alligator Attacks")
 print(plot)
 
-ggsave("ex44Histogram.eps", width = 5, height = 3)
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex45a.eps", width = 4, height = 2.5)
 
+plot <- ggplot(ex45, aes(x = year, y = bites)) + 
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Number of Bites") +
+  ggtitle("Alligator Attacks Over Time")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week01/hw/figures/ex45b.eps", width = 4, height = 2.5)
--- sccc/math146/week01/notes/prison_population.R
+++ sccc/math146/week01/notes/prison_population.R
@@ -203,83 +203,83 @@ state.rates <- function(df, target.year) {
     geom_point() +
     # geom_bar(stat = "identity", fill = "lightblue", color = "black") +
     labs(x = "State", y = "Rate") +
+with.percentages <- ddply(pbs, .(year, region), transform, 
+      percent.of.region = prison.population / sum(prison.population) * 100) 
+
+subset(with.percentages, year == 2010, select = c(state, region, percent.of.region))
+
+plot <- ggplot(percentages, 
+               aes(x = year, y = percent, fill = region, order = region)) + 
+  geom_bar(stat = "identity", color = "black") +
+  labs(x = "Region", y = "Percentage") +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  ggtitle("Regions over Time With Percentages") 
+  # scale_fill_grey() 
+print(plot)
+
+ggsave("figures/regions_over_time_percentages.eps", width = 5, height = 3)
+
+
+# 2010 by region
+by.region.2010 <- subset(cast(pbs.m, year + region ~ variable, sum), year == 2010)
+plot <- ggplot(by.region.2010, aes(x = reorder(region, -prison.population), y = prison.population / 1000)) + 
+  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
+  ggtitle("2010 by Region")
+print(plot)
+
+ggsave("figures/regions_2010.eps", width = 5, height = 3)
+
+
+# plots the populations for states in a region
+region.by.state <- function(df, target.region, target.year) {
+  by.state <- subset(cast(pbs.m, year + region + state ~ variable, sum), 
+                     year == target.year & region == target.region)
+
+  plot <- ggplot(by.state, aes(x = reorder(state, prison.population), 
+                               y = prison.population / 1000)) + 
+    geom_bar(stat = "identity", fill = "lightblue", color = "black") +
+    labs(x = "State", y = "Population (thousands)") +
     coord_flip() +
-    ggtitle(paste(target.year, "Rates"))
+    ggtitle(paste(target.region, "by State"))
   print(plot)
+
   plot
 }
 
-state.rates(with.rate.ranks, 1980)
-ggsave("figures/state_rates_1980.eps", width = 5, height = 7)
+plot <- region.by.state(pbs, "West", 2010)
+ggsave("figures/west_by_state.eps", width = 5, height = 3)
 
-state.rates(with.rate.ranks, 2010)
-ggsave("figures/state_rates_2010.eps", width = 5, height = 7)
+# *** population histograms ***
+population.histogram <- function(df, target.year, bw) {
+  plot <- ggplot(data = subset(df, year == target.year), 
+                 aes(x = prison.population / 1000)) + 
+    geom_histogram(fill = "lightblue", color = "black", binwidth = bw) +
+    labs(x = "Population (thousands)", y = "Number of States") +
+    ggtitle(paste(target.year, "Prison Population"))
+  print(plot)
 
-# regional rates
-with.rates <- add.rates(cast(pbs.m, year + region ~ variable, sum))
+  plot
+}
 
-plot <- ggplot(subset(with.rates, year == 2010), 
-               aes(x = reorder(region, -rate), y = rate)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Region", y = "Rate") +
-  ggtitle("2010 Regional Rates")
+population.histogram(pbs, 1980, 2)
+ggsave("figures/population_histogram_1980.eps", width = 5, height = 3)
+
+population.histogram(pbs, 2010, 10)
+ggsave("figures/population_histogram_2010_small_bins.eps", width = 5, height = 3)
+
+# all years doesn't make much sense
+plot <- ggplot(pbs, aes(x = prison.population / 1000)) + 
+  geom_histogram(fill = "lightblue", color = "black", binwidth = 10) +
+  labs(x = "Population (thousands)", y = "Number of States") +
+  ggtitle("1978 - 2012 Prison Population")
 print(plot)
 
-ggsave("figures/regional_rates_2010.eps", width = 5, height = 3)
+# *** incarceration rates ***
+rate.histogram <- function(df, target.year, bw) {
+  plot <- ggplot(data = subset(add.rates(df), year == target.year), aes(x = rate)) + 
+    geom_histogram(fill = "lightblue", color = "black", binwidth = bw) +
+    labs(x = "Rate", y = "Number of States") +
+    ggtitle(paste(target.year, "Incarceration Rate"))
+  print(plot)
 
-# US rate
-with.rates <- add.rates(cast(pbs.m, year ~ variable, sum))
-
-plot <- ggplot(with.rates, aes(x = year, y = rate)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Year", y = "Rate") +
-  ggtitle("US Incarceration Rate")
-print(plot)
-ggsave("figures/us_rate_over_time.eps", width = 5, height = 3)
-
-
-# WA rate
-plot <- ggplot(subset(add.rates(pbs), state == "Washington"), aes(x = year, y = rate)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Year", y = "Rate") +
-  ggtitle("WA Incarceration Rate")
-print(plot)
-
-ggsave("figures/wa_rate_over_time.eps", width = 5, height = 3)
-
-# world incarceration rates
-world.rates <- read.delim("data/world_incarceration_rates.txt", header = TRUE, 
-                          strip.white = TRUE, sep = '\t')
- 
-plot <- ggplot(data = world.rates, aes(x = rate)) + 
-  geom_histogram(fill = "lightblue", color = "black", binwidth = 100) +
-  labs(x = "Incarceration Rate", y = "Number Countries") +
-  ggtitle("World Incarceration Rates")
-print(plot)
-
-ggsave("figures/wold_histogram.eps", width = 5, height = 3)
-
-
-sample.rates <- subset(world.rates, 
-                       country == "United States" | 
-                       country == "Cuba" | 
-                       country == "South Africa" | 
-                       country == "England and Wales" | 
-                       country == "France" | 
-                       country == "Japan" |
-                       country == "India" | 
-                       country == "South Africa") 
-
-plot <- ggplot(sample.rates, aes(x = reorder(country, rate), y = rate)) + 
-  geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-  labs(x = "Country", y = "Rate") +
-  coord_flip() +
-  ggtitle("World Incarceration Rates")
-print(plot)
-ggsave("figures/sample_world_rates.eps", width = 5, height = 3)
-
-pbs
-sink("data_2010.tex")
-xtable(subset(pbs, year == 2010))
-sink()
-
+  plot
