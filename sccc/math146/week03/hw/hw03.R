6da6e685f7e3e2de8823cd9c88ed0fc4eddfedf9
Edwin Tellman <etellman@pobox.com>
2014-01-21 19:52:55 -0800

    hw 3 solutions

--- sccc/math146/week03/hw/hw03.R
+++ sccc/math146/week03/hw/hw03.R
@@ -10,9 +10,10 @@ ex25.range(373, 67)
 pnorm(70, 100, 15)
 
 # exercise 28
-plot <- ggplot(data.frame(x = c(-3, 3)), aes(x = x)) +
-  stat_function(fun = dnorm) +
-  stat_function(fun = limit.domain(dnorm, 1.77, 3), geom = "area", fill = "lightblue") +
+plot <- ggplot(data.frame(x = c(-25, 525)), aes(x = x)) +
+  stat_function(fun = function(x) dnorm(x, 250, 176)) +
+  stat_function(fun = limit.domain(function(x) dnorm(x, 250, 176), -25, 25), geom = "area", fill = "lightblue") +
+  stat_function(fun = limit.domain(function(x) dnorm(x, 250, 176), 475, 525), geom = "area", fill = "lightblue") +
   ggtitle("Exercise 28 (c)")
 print(plot)
 
@@ -23,12 +24,15 @@ qnorm(0.1, 100, 50)
 pnorm(5, 5.4, 0.54)
 pnorm((5 - 5.4)/0.54)
 
-z <- (25 - 18.7)/4.3
+z <- (750 - 499)/110
 round(z, digits = 4)
 
-round(pnorm(z), digits = 4)
+round(1 - pnorm(750, 499, 110), digits = 4)
+round(1 - pnorm(2.2818), digits = 4)
 
-qnorm(0.9)
-qnorm(0.9, 18.7, 4.3)
+pnorm(500, 250, 175.78)
+round(qnorm(c(0.1, 0.9), 250, 175.78), digits = 2)
 
-18.7 + 1.2816 * 4.3
+round((750 - 533)/116, digits = 4)
+
+(475-250)/1.28
