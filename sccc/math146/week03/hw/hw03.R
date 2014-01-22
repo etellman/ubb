4f4b76e255aaf05337fa5eb315afea28a1141145
Edwin Tellman <etellman@pobox.com>
2014-01-21 18:01:24 -0800

    hw 3 solutions

--- sccc/math146/week03/hw/hw03.R
+++ sccc/math146/week03/hw/hw03.R
@@ -18,7 +18,17 @@ print(plot)
 
 qgsave("~/Documents/U/ubb/sccc/math146/week03/hw/figures/ex28c.eps", width = 4, height = 2.5)
 
-# exercise 29
+# exercise 30
 qnorm(0.1, 100, 50)
-pnorm(.8416212)
+pnorm(5, 5.4, 0.54)
+pnorm((5 - 5.4)/0.54)
 
+z <- (25 - 18.7)/4.3
+round(z, digits = 4)
+
+round(pnorm(z), digits = 4)
+
+qnorm(0.9)
+qnorm(0.9, 18.7, 4.3)
+
+18.7 + 1.2816 * 4.3
