4c3dc080b769b87c7c11ae5a60e9f7390b658131
Edwin Tellman <etellman@pobox.com>
2014-01-28 07:41:12 -0800

    hw 4

--- sccc/math146/week04/hw/hw04.R
+++ sccc/math146/week04/hw/hw04.R
@@ -1,66 +1,44 @@
 
-# exercise 25
-ex25.range <- function(mean, sd) {
-  c(mean - 2 * sd, mean + 2 * sd)
-}
+# exercise 26
+ex26 <- read.delim("ex04-26.dat", header = TRUE, sep = '\t')
+str(ex26)
 
-ex25.range(373, 67)
+plot <- ggplot(ex26, aes(x = Women, y = Men)) + 
+  geom_point() +
+  labs(x = "Women", y = "Men") +
+  # scale_x_continuous(limits = c(-10, 30)) +
+  ggtitle("Exercise 26: Date Heights")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex26.eps", width = 4, height = 2.5)
+
+with(ex26, round(cor(Women, Men), 4))
 
 # exercise 27
-pnorm(70, 100, 15)
+ex27 <- read.delim("ex04-27.dat", header = TRUE, sep = '\t')
+str(ex27)
+
+plot <- ggplot(ex27, aes(x = Price, y = Deforest)) + 
+  geom_point() +
+  labs(x = "Coffee Price", y = "Forest Lost") +
+  # scale_x_continuous(limits = c(-10, 30)) +
+  ggtitle("Exercise 27: Coffee and Deforestation")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex27.eps", width = 4, height = 2.5)
+
+with(ex27, round(cor(Price, Deforest), 4))
 
 # exercise 28
-plot <- ggplot(data.frame(x = c(-25, 525)), aes(x = x)) +
-  stat_function(fun = function(x) dnorm(x, 250, 176)) +
-  stat_function(fun = limit.domain(function(x) dnorm(x, 250, 176), -25, 25), geom = "area", fill = "lightblue") +
-  stat_function(fun = limit.domain(function(x) dnorm(x, 250, 176), 475, 525), geom = "area", fill = "lightblue") +
-  ggtitle("Exercise 28 (c)")
+ex28 <- read.delim("ex04-28.dat", header = TRUE, sep = '\t')
+str(ex28)
+
+plot <- ggplot(ex28, aes(x = Newbirds, y = Pctret)) + 
+  geom_point() +
+  labs(x = "Percent Returning", y = "New Birds") +
+  ggtitle("Exercise 28: Birds")
 print(plot)
 
-qgsave("~/Documents/U/ubb/sccc/math146/week03/hw/figures/ex28c.eps", width = 4, height = 2.5)
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex28.eps", width = 4, height = 2.5)
 
-# exercise 46
-ex46 <- read.delim("ex03-46.dat", header = TRUE, sep = '\t')
-str(ex46)
-
-plot <- ggplot(ex46, aes(x = IQ)) + 
-  geom_histogram(fill = "lightblue", color = "black", binwidth = 10) +
-  labs(x = "IQ", y = "Count") +
-  ggtitle("IQ")
-print(plot)
-
-ggsave("~/Documents/U/ubb/sccc/math146/week03/hw/figures/ex46.eps", width = 4, height = 2.5)
-
-iq <- ex46$IQ
-
-round(mean(iq), digits = 2)
-round(sd(iq), digits = 2)
-
-ex46.expected.1sd <- round( .68 * 31 )
-ex46.actual.1sd <- length(iq[mean(iq) - 1 * sd(iq) < iq & iq < mean(iq) + 1 * sd(iq)])
-
-ex46.expected.2sd <- round( .95 * 31 )
-ex46.actual.2sd <- length(iq[mean(iq) - 2 * sd(iq) < iq & iq < mean(iq) + 2 * sd(iq)])
-
-ex46.expected.1sd
-ex46.actual.1sd
-
-ex46.expected.2sd
-ex46.actual.2sd
-
-round(mean(iq) - 2 * sd(iq), digits = 2)
-
-# exercise 48
-ex48 <- c(4.33, 5.05, 5.44, 5.79, 6.81)
-diff(ex48)
-
-z1 <- round((5.05 - 5.43)/0.54, digits = 4)
-round(pnorm(z2), 4)
-
-z2 <- round((5.79 - 5.43)/0.54, digits = 4)
-round(pnorm(z2), 4)
-z2
-
-pnorm(5.05, 5.43, 0.54)
-pnorm(5.79, 5.43, 0.54)
-
+with(ex28, round(cor(Pctret, Newbirds), 4))
