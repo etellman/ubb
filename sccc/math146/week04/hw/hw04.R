b0752a289b27db322fcad357e3c7c263f5ce12e7
Edwin Tellman <etellman@pobox.com>
2014-01-28 22:10:03 -0800

    hw 4 solutions

--- sccc/math146/week04/hw/hw04.R
+++ sccc/math146/week04/hw/hw04.R
@@ -42,3 +42,80 @@ print(plot)
 ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex28.eps", width = 4, height = 2.5)
 
 with(ex28, round(cor(Pctret, Newbirds), 4))
+
+# exercise 29
+ex29 <- read.delim("ex04-29.dat", header = TRUE, sep = '\t')
+str(ex29)
+
+plot <- ggplot(ex29, aes(x = Neural, y = Behave)) + 
+  geom_point() +
+  labs(x = "Neural Activity", y = "Behavior") +
+  ggtitle("Exercise 29: Brain Activity")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex29.eps", width = 4, height = 2.5)
+
+with(subset(ex29, Neural < 100), round(cor(Behave, Neural), 4))
+
+# exercise 31
+ex31 <- read.delim("ta04-02.dat", header = TRUE, sep = '\t')
+str(ex31)
+
+plot <- ggplot(ex31, aes(x = Time, y = Length, shape = factor(Run))) + 
+  geom_point() +
+  labs(x = "Time", y = "Length") +
+  ggtitle("Exercise 31: Icicles")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex31.eps", width = 4, height = 2.5)
+
+ddply(ex31, "Run", function(df) c(cor = round(cor(df$Time, df$Length), 4)))
+
+# exercise 32
+ex32 <- read.delim("ex04-32.dat", header = TRUE, sep = '\t')
+str(ex32)
+
+plot <- ggplot(ex32, aes(x = Plants, y = Yield)) + 
+  geom_point() +
+  stat_summary(fun.y = "mean", geom = "line") +
+  stat_summary(fun.y = "mean", geom = "point", shape = 21, size = 2) +
+  labs(x = "Plants/Acre", y = "Yield") +
+  ggtitle("Exercise 32: Plant Yields")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex32.eps", width = 4, height = 2.5)
+
+with(ex32, round(cor(Plants, Yield), 4))
+
+# exercise 34
+ex34 <- read.delim("ex04-26.dat", header = TRUE, sep = '\t')
+str(ex34)
+
+cor(ex34$Women + 3, ex34$Women)
+cor(ex34$Men, ex34$Women)
+
+# exercise 43
+ex43 <- read.delim("ex04-43.dat", header = TRUE, sep = '\t')
+str(ex43)
+
+plot <- ggplot(ex43, aes(x = DD, y = Gas, shape = Data)) + 
+  geom_point() +
+  labs(x = "Degree Days", y = "Gas Usage") +
+  ggtitle("Exercise 43: Solar Panels")
+print(plot)
+
+ggsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex43.eps", width = 4, height = 2.5)
+
+# exercise 44
+ex44 <- read.delim("ex04-44.dat", header = TRUE, sep = '\t')
+str(ex44)
+
+plot <- ggplot(ex44, aes(x = Pairs, y = Pct)) + 
+  geom_point() +
+  labs(x = "Breeding Pairs", y = "Percent Returning") +
+  ggtitle("Exercise 44: Merlins Breeding")
+print(plot)
+
+round(cor(ex44$Pairs, ex44$Pct), 4)
+qgsave("~/Documents/U/ubb/sccc/math146/week04/hw/figures/ex44.eps", width = 4, height = 2.5)
+
