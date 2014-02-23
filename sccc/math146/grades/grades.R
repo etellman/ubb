571ecc7758d72b32c245b2b1a4f816ac3cd941ca
Ed Tellman <etellman@pobox.com>
2014-02-22 20:48:11 -0800

    week 4 grades

--- sccc/math146/grades/grades.R
+++ sccc/math146/grades/grades.R
@@ -9,8 +9,8 @@ percentage <- function(score, points) {
   c(actual = actual, percentage = round(100 * (actual) / points))
 }
 
-possible <- points(c(25:27, 29:30, 37:39, 41:43, 45, 50:51))
-percentage(33, possible)
+possible <- points(c(25:29, 31:32, 34, 36:39, 43:44))
+percentage(5, possible)
 
 setwd("~/Documents/U/ubb/sccc/math146/grades")
 grades <- read.delim("grades.csv", header = TRUE, strip.white = T, sep = ',')
