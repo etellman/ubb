ff920be39e392068f7680f31225ae3bad336261d
Ed Tellman <etellman@pobox.com>
2014-03-16 09:24:39 -0700

    inactive student list

--- sccc/math146/grades/grades.r
+++ sccc/math146/grades/grades.r
@@ -1,3 +1,5 @@
+grades.dir <- '~/Documents/U/ubb/sccc/math146/grades'
+
 # returns the number of points for a list of problems
 points <- function(problems) {
   5 * length(problems)
@@ -12,22 +14,18 @@ percentage <- function(score, points) {
 possible <- points(c(19:24, 27, 29:31))
 percentage(6, possible)
 
-grades.dir <- '~/Documents/U/ubb/sccc/math146/grades'
 grades.file <- paste(grades.dir, 'grades.csv', sep = '/')
 grades <- read.delim(grades.file, header = TRUE, strip.white = T, sep = ',')
 
 grades.m <- melt(grades, id = "id")
 
-grades.m
-
-round(summary(grades.m$value, na.rm = T))
-
-hw5 <- subset(grades.m, variable == 'hw5', select = c('id', 'value'))
-hw5$id
+exam1.summary <- summary(subset(grades, select = 'exam1'), na.rm = T)
+exam1.summary
 
 data.frame(hw5$id, round(scale(hw5$value), 2))
 
-plot <- ggplot(grades.m, aes(x = value)) + 
+plot <- ggplot(grades, aes(x = exam1)) + 
   geom_histogram(binwidth = 5, color = "black", fill = "lightblue") +
   labs(x = "Grade", y = "Students")
 print(plot)
+
