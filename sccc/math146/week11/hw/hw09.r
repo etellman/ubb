a78139d4944f0e04aa2b621dc6484c9a66278c07
Ed Tellman <etellman@pobox.com>
2014-03-25 19:58:03 -0700

    week 11 hw

---
+++ sccc/math146/week11/hw/hw09.r
@@ -0,0 +1,15 @@
+
+root.dir <- "~/Documents/U/ubb/sccc/math146"
+
+data.dir <- paste(root.dir, "data/bps/PC-Text", sep = "/")
+hw9.data.dir <- paste(data.dir, "ch09", sep = "/")
+
+hw.dir <- paste(root.dir, "week09/hw", sep = "/")
+figures.dir <- paste(hw.dir, "figures", sep = "/")
+
+setwd(hw9.data.dir)
+
+1 - 0.13 
+
+
+1 - 4176000/9094000 
