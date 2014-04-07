9c4a50c5e48ddfcaec993665ed9ee03bc1e2c00b
Ed Tellman <etellman@pobox.com>
2014-04-07 08:31:13 -0700

    week 12 stuff

--- sccc/math146/week12/hw/hw10.r
+++ sccc/math146/week12/hw/hw10.r
@@ -26,3 +26,38 @@ round((71 - 70) / (0.56), 4)
 round(pnorm(71, 70, 0.56), 4)
 round(pnorm(69, 70, 0.56), 4)
 round(pnorm(71, 70, 0.56) - pnorm(69, 70, 0.56), 4)
+
+# exercise 27
+x <- pnorm(140, 125, 5)
+round(1 - x, 4)
+
+# exercise 28
+x <- qnorm(0.95, 0, 1)
+x <- qnorm(0.95, 125, 5)
+
+round(x, 4)
+
+5 * (1.6449) + 125
+
+# exercise 30
+s <- 1.4 / sqrt(52)
+round(pnorm(100/52, 2.2, s), 4)
+100/52
+
+# exercise 31
+u <- qnorm(0.99, 0, 1)
+round(u, 4)
+round(u * 0.01 + .2, 4)
+
+round(qnorm(.99, 0.2, 0.01), 4)
+
+# exercise 32
+s <- 20.2/sqrt(40)
+z <- (5 - 8.7)/3.1939
+round(z, 4)
+
+round(1 - pnorm(0.407, 0, 1), 4)
+round(pnorm(-1.1585, 0, 1), 4)
+
+# exercise 38
+18.96 / sqrt(14000)
