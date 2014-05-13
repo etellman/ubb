f84ee357643b322f3162021cad86f0189cf7e8df
Ed Tellman <etellman@pobox.com>
2014-05-12 21:30:10 -0700

    hw 10 grades

--- sccc/math146/grades/grades.r
+++ sccc/math146/grades/grades.r
@@ -11,8 +11,8 @@ percentage <- function(score, points) {
   c(actual = actual, percentage = round(100 * (actual) / points))
 }
 
-possible <- points(c(29:32, 34, 36, 38:41, 43:44, 48))
-percentage(3, possible)
+possible <- points(c(22:27, 29, 31:33, 35:40))
+percentage(1, possible)
 
 grades.file <- paste(grades.dir, 'grades.csv', sep = '/')
 grades <- read.delim(grades.file, header = TRUE, strip.white = T, sep = ',')
--- sccc/math146/week15/notes/notes15.r
+++ sccc/math146/week15/notes/notes15.r
@@ -1,91 +1,22 @@
 root.dir <- '~/Documents/U/ubb/sccc/math146'
-notes.dir <- paste(root.dir, "week13/notes", sep = "/")
-setwd(notes.dir)
 
-10 * .1^2 * .9^3
+notes.dir <- paste(root.dir, "week14/notes", sep = "/")
+data.dir <- paste(root.dir, "data/bps/PC-Text/ch14", sep = "/")
+setwd(data.dir)
 
-choose(10, 2) * 1/6^2 * (5/6)^8
+60/sqrt(840)
+272+4.14
 
-(5/6)^10
+x <- (1 - 0.975)/2
+qnorm(c(x, 1 - x))
 
-choose(100, 9) * 0.1^9 * 0.9^91
+# ex 14.4
+5 + 0.2 * qnorm(0.95)
 
-1 - 0.31 - 0.19
+# ex 14.5
+ex5 <- read.delim("ex14-05.dat", header = TRUE, sep = '\t')
 
-.5^9
+str(ex5)
+mean(ex5$IQ) - 2.576 * 15/sqrt(31)
 
-pnorm(1) - pnorm(-1)
-pnorm(-1)
-pnorm(1)
-
-s <- sqrt(90)
-1 - (pbinom(103.5, 1000, 0.1) - pbinom(96.5, 1000, 0.1))
-
-pbinom(15, 25, 0.6)
-
-sqrt(0.6 * 25 * 0.4)
-
-1 - pnorm(14.5, 15, 2.449)
-
-sum(dbinom(97:103, 1000, .1))
-
-pbinom(103.5, 1000, 0.1) - pbinom(96.5, 1000, 0.1)
-pnorm(1) - pnorm(-1)
-
-choose(1000, 100) * 0.1^100 * .9^900
-
-s <- sqrt(100 * .9)
-s
-
-3/s
-pnorm(3/s)
-pnorm(3.5/s) - pnorm(-3.5/s)
-
-sum(dbinom(90:110, 1000, 0.1))
-
-m <- 90 * 0.477
-s <- sqrt(90 * 0.477 * (1 - 0.477))
-
-z <- (29.5 - m)/s
-
-pnorm(z)
-
-pbinom(29, 90, 0.477)
-
-qbinom(5.1, 10, .5)
-
-1 - pnorm((1520 - 1500)/24.49)
-
-# ex 11
-m <- 0.27 * 1535
-print(m)
-s <- sqrt(m * (1 - .27))
-s
-
-1 - pbinom(415, 1535, 0.27)
-
-z <- (416 - m)/s
-1 - pnorm(z)
-
-# ex 12
-m <- 0.12 * 1500
-m
-s <- sqrt(m * (1 - .12))
-s
-
-z1 <- (c(165,195) - m)/s
-z1
-
-pnorm(z1[[2]]) - pnorm(z1[[1]])
-
-z <- (416 - m)/s
-1 - pnorm(z)
-
-.12 * 1500
-
-1 - sum(dbinom(0:3, 15, 0.2))
-
-choose(10, 0:3) * .7^7 * .3^3
-
-# ex 8
-sqrt(15 * 0.7 * 0.3)
+105.
