cdd66eb9e9fbbb3f360ab977388479cf7574cdf0
Ed Tellman <etellman@pobox.com>
2014-06-03 21:54:23 -0700

    hw 12 grades

--- sccc/math146/grades/grades.r
+++ sccc/math146/grades/grades.r
@@ -11,8 +11,8 @@ percentage <- function(score, points) {
   c(actual = actual, percentage = round(100 * (actual) / points))
 }
 
-possible <- points(c(22:27, 29, 31:33, 35:40))
-percentage(3, possible)
+possible <- points(c(22:26, 28, 31:32, 34:36, 38:39))
+percentage(6, possible)
 
 possible
 
