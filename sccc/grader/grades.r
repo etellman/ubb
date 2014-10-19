bf9bd81fc7dfa29cbf0bcf5593c53c836728962c
Ed Tellman <etellman@pobox.com>
2014-10-18 19:47:23 -0700

    exam 1 grades

--- sccc/grader/grades.r
+++ sccc/grader/grades.r
@@ -11,17 +11,23 @@ percentage <- function(score, points) {
   c(actual = actual, percentage = round(100 * (actual) / points))
 }
 
-possible <- points(c(1, 5:8, 13:15, 18:19, 21:22, 25:32, 37:38, 40, 43, 46))
-percentage(4, possible)
-
-possible
+possible <- points(c(1, 3:9, 14, 19:28, 35:37, 41:43))
+percentage(10, possible)
 
 grades.file <- paste(grades.dir, 'grades.csv', sep = '/')
 grades <- read.delim(grades.file, header = TRUE, strip.white = T, sep = ',')
 
 grades.m <- melt(grades, id = "id")
-
 median(grades.m$value, na.rm = T)
+round(mean(grades.m$value, na.rm = T))
+
+hw <- grades.m[grep("hw", grades.m$variable),]
+exam01 <- grades.m[grep("exam", grades.m$variable),]
+
+exam01.mean <- mean(exam01$value, na.rm = T)
+hw.mean <- mean(hw$value, na.rm = T)
+
+0.8 * exam01.mean + 0.2 * hw.mean
 
 plot <- ggplot(grades.m, aes(x = value)) + 
   geom_histogram(binwidth = 5, color = "black", fill = "lightblue") +
