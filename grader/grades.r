45b5eab786beaf6174a2df5ea94187e32aa24244
Ed Tellman <etellman@pobox.com>
2015-02-21 17:22:07 -0800

    grades file

--- grader/grades.r
+++ grader/grades.r
@@ -7,12 +7,14 @@ points <- function(problems) {
 
 # returns the percentage given a number of points wrong and total number of points
 percentage <- function(score, points) {
-  actual <- points - score
+  actual <- points + score
   c(actual = actual, percentage = round(100 * (actual) / points))
 }
 
-possible <- points(c(3:5, 11:16, 20:25, 30:36, 39:40, 45, 49, 53))
-percentage(6, possible)
+possible <- points(c(6:10, 15:20, 27:28, 30, 33, 43, 1, 5, 24, 28, 32, 35, 36, 52, 54, 57))
+percentage(-3, possible)
+
+possible
 
 grades.file <- paste(grades.dir, 'grades.csv', sep = '/')
 grades <- read.delim(grades.file, header = TRUE, strip.white = T, sep = ',')
@@ -25,7 +27,7 @@ hw <- grades.m[grep("hw", grades.m$variable),]
 exam01 <- grades.m[grep("exam", grades.m$variable),]
 
 plot <- ggplot(grades.m, aes(x = value)) + 
-  geom_histogram(binwidth = 5, origin = 50.1, color = "black", fill = "lightblue") +
+  geom_histogram(binwidth = 5, origin = 40.1, color = "black", fill = "lightblue") +
   labs(x = "Grade", y = "Students")
 print(plot)
 
