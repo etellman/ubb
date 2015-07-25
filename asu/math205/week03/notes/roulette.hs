b5ebf57fe5f766119b4c8d7e9aa53ace0336c5a0
Ed Tellman <etellman@pobox.com>
2015-07-25 18:33:23 +0200

    wheel data type

--- asu/math205/week03/notes/roulette.hs
+++ asu/math205/week03/notes/roulette.hs
@@ -2,28 +2,44 @@ import System.Random
 
 main = do
   gen <- getStdGen 
-  let rolls = randomRs (0, 37) gen :: [Int]
-  -- let results = fixedNumGames oneNumber 50 20 rolls
-  let results = fixedAmount oneNumber 10 20 rolls
-  putStrLn "games"
+  let infiniteRolls = randomRs (0, 37) gen :: [Int]
+  let results = fixedNumGames (Wheel oddEven infiniteRolls) 50 20 
+
+  -- let wheel = Wheel { bettingStrategy = oddEven, rolls = infiniteRolls }
+  -- let (amount, newWheel) = spin wheel
+  -- print amount
+
+  -- let results = fixedAmount oneNumber 10 20 rolls
+  putStrLn "delta.money"
   printResults results
+
   
 type Roll = Int
 type Rolls = [Roll]
 type Money = Int
-type Roulette = Roll -> Money
-data Wheel = Wheel { bettingStrategy :: Roulette, rolls :: [Int] }
+type Roulette = Roll -> Money 
+data Wheel = Wheel { bettingStrategy :: Roulette, rolls :: [Int] } 
+data Player = Player { cash :: Money, gamesPlayed :: Int } 
 
 spin :: Wheel -> (Money, Wheel)
-spin wheel(bettingStrategy, rolls) = (0, Wheel(bettingStrategy, tail rolls))
+spin (Wheel bettingStrategy rolls) = 
+  (bettingStrategy (head rolls), 
+    Wheel { bettingStrategy = bettingStrategy, rolls = (tail rolls) })
 
-fixedNumGames :: Roulette -> Int -> Int -> Rolls -> [Int]
-fixedNumGames roulette gamesPerPlayer players rolls 
+fixedNumGames :: Wheel -> Int -> Int -> [Money]
+fixedNumGames wheel gamesPerPlayer players 
   | players == 0 = []
   | otherwise = nextPlayerResult : 
-                (fixedNumGames roulette gamesPerPlayer (players - 1) nextRolls) 
-      where nextPlayerResult = games roulette rolls 50
-            nextRolls = drop gamesPerPlayer rolls
+                (fixedNumGames nextWheel gamesPerPlayer (players - 1)) 
+      where (nextPlayerResult, nextWheel) = games wheel 50
+
+-- play the specified number of games and return the amount of money remaining
+games :: Wheel -> Int -> (Money, Wheel)
+games wheel numGames 
+  | numGames == 0 = (0, wheel)
+  | otherwise = (bet + otherBets, lastWheel)
+      where (bet, nextWheel) = spin wheel
+            (otherBets, lastWheel) = games nextWheel (numGames - 1)
 
 -- play with some number of players, each with a fixed amount of money, returning the 
 -- number of games it took them to loose all their money for each player
@@ -38,12 +54,6 @@ spendAll :: Roulette -> Money -> Rolls -> Int
 spendAll roulette cash rolls 
   | cash == 0 = 0
   | otherwise = 1 + spendAll roulette (cash + roulette(rolls !! 0)) (tail rolls)  
-
--- play the specified number of games and return the amount of money remaining
-games :: Roulette -> Rolls -> Int -> Money
-games roulette rolls numGames 
-  | numGames == 0 = 0
-  | otherwise = roulette(rolls !! 0) + games roulette (tail rolls) (numGames - 1) 
 
 -- play one game of roulette
 roulette :: Int -> Money -> Roll -> Money
