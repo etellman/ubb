380af2f799bf1aa4ee8d4ef6f7c4f8f2ac6a74d6
Ed Tellman <etellman@pobox.com>
2015-07-27 15:28:05 +0200

    week 8

---
+++ asu/math205/week08/hw/hw07.r
@@ -0,0 +1,16 @@
+root.dir <- '~/Documents/U/ubb/sccc/math146'
+
+data.dir <- paste(root.dir, 'data/bps/PC-Text', sep = '/')
+hw7.data.dir <- paste(data.dir, 'ch07', sep = '/')
+
+hw.dir <- paste(root.dir, 'week07/hw', sep = '/')
+figures.dir <- paste(hw.dir, 'figures', sep = '/')
+
+setwd(hw7.data.dir)
+
+# exercise 33
+(2000 - 831)/2000
+2000 - 831
+
+# exercise 35
+(45956 - 5029) / 45956
